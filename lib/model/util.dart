4374d66f945930aa58412298b011a851470aa704
mjiang24 <mjiang24@illinois.edu>
2020-03-14 23:10:15 -0500

    place holder card

--- lib/model/util.dart
+++ lib/model/util.dart
@@ -13,3 +13,35 @@ class LinkTextSpan extends TextSpan {
       }
   );
 }
+
+class PlaceHolderCard extends StatelessWidget {
+
+  const PlaceHolderCard(
+  {Key key,
+    this.text,
+    this.height,
+  }) : super(key: key);
+
+  final String text;
+  final height;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+      padding: EdgeInsets.only(left: 24, right: 24),
+      child: Card(
+        child: Container(
+          height: height,
+          child: Center(
+            child: Container(
+              child: Text(
+                  text
+              ),
+            ),
+          ),
+        )
+      ),
+    );
+  }
+}
