de203176c4815bb0fa87bbf7a3a1c22240109057
mjiang24 <mjiang24@illinois.edu>
2020-04-13 23:00:33 -0500

    refactor setting first step

--- lib/views/settings/settings.dart
+++ lib/views/settings/settings.dart
@@ -6,185 +6,100 @@ import 'package:freetitle/views/reset_email/reset_email.dart';
 import 'package:freetitle/views/reset_password/reset_password.dart';
 import 'package:freetitle/views/reset_username/reset_username.dart';
 
-//class SettingTab extends StatelessWidget{
-//  const SettingTab(
-//  {Key key,
-//    this.text,
-//    this.callback,
-//    this.textColor,
-//  }) : super(key: key);
-//
-//  final String text;
-//  final textColor;
-//  final VoidCallback callback;
-//  @override
-//  Widget build(BuildContext context) {
-//    return Padding(
-//      padding: EdgeInsets.all(8),
-//      child: Container(
-//          height: 50,
-//          decoration: BoxDecoration(
-//            color: AppTheme.white,
-//            borderRadius: BorderRadius.only(
-//                topLeft: Radius.circular(8.0),
-//                bottomLeft: Radius.circular(8.0),
-//                bottomRight: Radius.circular(8.0),
-//                topRight: Radius.circular(8.0)),
-//            boxShadow:  <BoxShadow>[
-//              BoxShadow(
-//                  color: AppTheme.grey.withOpacity(0.2),
-//                  offset: Offset(1.1, 1.1),
-//                  blurRadius: 10.0
-//              ),
-//            ],
-//          ),
-//          child: Material(
-//            child: InkWell(
-//              onTap: () {
-//                callback();
-//                Navigator.pop(context);
-//              },
-//              child: Center(
-//                child: Text(
-//                  text,
-//                  style: TextStyle(
-//                      color: textColor
-//                  ),
-//                ),
-//              ),
-//            ),
-//          )
-//      ),
-//    );
-//  }
-//}
+class SettingTab extends StatelessWidget{
+  const SettingTab(
+  {Key key,
+    @required this.text,
+    @required this.callback,
+    this.textColor,
+  }) : super(key: key);
 
-class SettingsPage extends StatelessWidget{
+  final String text;
+  final textColor;
+  final VoidCallback callback;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: Container(
+          height: 50,
+          decoration: BoxDecoration(
+            color: AppTheme.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(8.0),
+                bottomLeft: Radius.circular(8.0),
+                bottomRight: Radius.circular(8.0),
+                topRight: Radius.circular(8.0)),
+            boxShadow:  <BoxShadow>[
+              BoxShadow(
+                  color: AppTheme.grey.withOpacity(0.2),
+                  offset: Offset(1.1, 1.1),
+                  blurRadius: 10.0
+              ),
+            ],
+          ),
+          child: Material(
+            child: InkWell(
+              onTap: () {
+                callback();
+              },
+              child: Center(
+                child: Text(
+                  text,
+                  style: TextStyle(
+                      color: textColor
+                  ),
+                ),
+              ),
+            ),
+          )
+      ),
+    );
+  }
+}
+
+class SettingsPage extends StatefulWidget {
+
+
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+
+  void getResetPassword() {
+    Navigator.push<dynamic>(
+      context,
+      MaterialPageRoute(builder: (context) {
+        return ResetPasswordScreen();
+      }),
+    );
+  }
+
+  void getResetEmail() {
+    Navigator.push<dynamic>(
+      context,
+      MaterialPageRoute(builder: (context) {
+        return ResetEmailScreen();
+      }),
+    );
+  }
+
+  void getResetUsername() {
+    Navigator.push<dynamic>(
+      context,
+      MaterialPageRoute(builder: (context) {
+        return ResetUsernameScreen();
+      }),
+    );
+  }
 
 
   @override
   Widget build(BuildContext context) {
     List<Widget> settingsList = List();
-    settingsList.add(Padding(
-      padding: EdgeInsets.all(8),
-      child: Container(
-          height: 50,
-          decoration: BoxDecoration(
-            color: AppTheme.white,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(8.0),
-                bottomLeft: Radius.circular(8.0),
-                bottomRight: Radius.circular(8.0),
-                topRight: Radius.circular(8.0)),
-            boxShadow:  <BoxShadow>[
-              BoxShadow(
-                  color: AppTheme.grey.withOpacity(0.2),
-                  offset: Offset(1.1, 1.1),
-                  blurRadius: 10.0
-              ),
-            ],
-          ),
-          child: Material(
-            child: InkWell(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) {
-                    return ResetPasswordScreen();
-                  }),
-                );
-              },
-              child: Center(
-                child: Text(
-                  '修改密码',
-                  style: TextStyle(
-                      color: Colors.black
-                  ),
-                ),
-              ),
-            ),
-          )
-      ),
-    ));
-    settingsList.add(Padding(
-      padding: EdgeInsets.all(8),
-      child: Container(
-          height: 50,
-          decoration: BoxDecoration(
-            color: AppTheme.white,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(8.0),
-                bottomLeft: Radius.circular(8.0),
-                bottomRight: Radius.circular(8.0),
-                topRight: Radius.circular(8.0)),
-            boxShadow:  <BoxShadow>[
-              BoxShadow(
-                  color: AppTheme.grey.withOpacity(0.2),
-                  offset: Offset(1.1, 1.1),
-                  blurRadius: 10.0
-              ),
-            ],
-          ),
-          child: Material(
-            child: InkWell(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) {
-                    return ResetEmailScreen();
-                  }),
-                );
-              },
-              child: Center(
-                child: Text(
-                  '修改邮箱',
-                  style: TextStyle(
-                      color: Colors.black
-                  ),
-                ),
-              ),
-            ),
-          )
-      ),
-    ));
-    settingsList.add(Padding(
-      padding: EdgeInsets.all(8),
-      child: Container(
-          height: 50,
-          decoration: BoxDecoration(
-            color: AppTheme.white,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(8.0),
-                bottomLeft: Radius.circular(8.0),
-                bottomRight: Radius.circular(8.0),
-                topRight: Radius.circular(8.0)),
-            boxShadow:  <BoxShadow>[
-              BoxShadow(
-                  color: AppTheme.grey.withOpacity(0.2),
-                  offset: Offset(1.1, 1.1),
-                  blurRadius: 10.0
-              ),
-            ],
-          ),
-          child: Material(
-            child: InkWell(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) {
-                    return ResetUsernameScreen();
-                  }),
-                );
-              },
-              child: Center(
-                child: Text(
-                  '修改用户名',
-                  style: TextStyle(
-                      color: Colors.black
-                  ),
-                ),
-              ),
-            ),
-          )
-      ),
-    ));
+    settingsList.add(SettingTab(text: "修改密码", callback: getResetPassword,));
+    settingsList.add(SettingTab(text: '修改邮箱', callback: getResetEmail));
+    settingsList.add(SettingTab(text: '修改用户名', callback: getResetUsername));
     settingsList.add(SizedBox(height: 30,));
     settingsList.add(Padding(
       padding: EdgeInsets.all(8),
