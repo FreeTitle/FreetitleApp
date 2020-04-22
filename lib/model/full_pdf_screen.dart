49163d03e4ddfd1cc26095103bbb2d956fa92cf3
mjiang24 <mjiang24@illinois.edu>
2020-04-21 21:13:39 -0500

    add full pdf screen

---
+++ lib/model/full_pdf_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_full_pdf_viewer/full_pdf_viewer_scaffold.dart';
+
+class FullPDFScreen extends StatelessWidget {
+  FullPDFScreen({Key key, this.pdfPath, this.filename}) : super(key : key);
+
+  final String pdfPath;
+  final String filename;
+
+  @override
+  Widget build(BuildContext context) {
+    return PDFViewerScaffold(
+      appBar: AppBar(
+        brightness: Brightness.dark,
+        title: Text(filename, style: TextStyle(color: Colors.black),),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios, color: Colors.black,),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      path: pdfPath,
+    );
+  }
+}
