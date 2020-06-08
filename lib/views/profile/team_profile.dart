92f5bb6c539033f64dc6fddcad4de0cd6cfa75d1
Zihan Xu <zihanxu3@illinois.edu>
2020-06-07 19:51:37 -0500

    add groupView and bordered button

--- lib/views/profile/team_profile.dart
+++ lib/views/profile/team_profile.dart
@@ -1,7 +1,9 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:freetitle/views/settings/settings.dart';
 
 import 'my_profile.dart';
+import 'team_group.dart';
 
 import 'package:flutter/material.dart';
 import 'package:freetitle/app_theme.dart';
@@ -66,14 +68,15 @@ class MyViewState extends State<MyView> {
             fit: BoxFit.fill, // otherwise the logo will be tiny
             child: new Material(
               type: MaterialType.transparency,
-              child: Ink(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey[350], width: 5.0),
-                  shape: BoxShape.circle,
-                ),
+              child: DottedBorder(
+                borderType: BorderType.Circle,
+                radius: Radius.circular(15),
+                strokeWidth: 3,
+                dashPattern: [10, 8],
+                color: Theme.of(context).highlightColor, 
                 child: IconButton(
                   iconSize: 50,
-                  color: Colors.grey,
+                  color: Theme.of(context).highlightColor, 
                   highlightColor: Colors.grey,
                   icon: Icon(Icons.add),
                   onPressed: () {
@@ -100,14 +103,15 @@ class MyViewState extends State<MyView> {
             fit: BoxFit.fill, // otherwise the logo will be tiny
             child: new Material(
               type: MaterialType.transparency,
-              child: Ink(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey[350], width: 5.0),
-                  shape: BoxShape.circle,
-                ),
+              child: DottedBorder(
+                borderType: BorderType.Circle,
+                radius: Radius.circular(15),
+                strokeWidth: 3,
+                dashPattern: [10, 8],
+                color: Theme.of(context).highlightColor,
                 child: IconButton(
                   iconSize: 50,
-                  color: Colors.grey,
+                  color: Theme.of(context).highlightColor,
                   highlightColor: Colors.grey,
                   icon: Icon(Icons.remove),
                   onPressed: () {
@@ -176,83 +180,114 @@ class MyViewState extends State<MyView> {
               Scaffold(
                 backgroundColor: Theme.of(context).primaryColor,
                 body: SingleChildScrollView(
-                  child: Padding(
-                    padding: EdgeInsets.all(25),
-                    child: Container(
-                      child: Column(
-                        children: <Widget>[
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Row(
-                            children: <Widget>[
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Text("全部成员",
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyText1
-                                      .merge(TextStyle(
-                                          fontSize: 17,
-                                          fontWeight: FontWeight.bold))),
-                              Spacer(),
-                              Padding(
-                                padding: EdgeInsets.only(right: 10),
-                                child: Row(
-                                  children: <Widget>[
-                                    Icon(
-                                      Icons.person,
-                                      color: Colors.grey[600],
-                                      semanticLabel: "Number",
-                                    ),
-                                    Text(
-                                      userName.length.toString(),
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.normal),
-                                    ),
-                                  ],
+                    child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(left:25, right: 25, top: 25),
+                      child: Container(
+                        child: Column(
+                          children: <Widget>[
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Row(
+                              children: <Widget>[
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text("全部成员",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyText1
+                                        .merge(TextStyle(
+                                            fontSize: 17,
+                                            fontWeight: FontWeight.bold))),
+                                Spacer(),
+                                Padding(
+                                  padding: EdgeInsets.only(right: 10),
+                                  child: Row(
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.person,
+                                        color: Colors.grey[600],
+                                        semanticLabel: "Number",
+                                      ),
+                                      Text(
+                                        userName.length.toString(),
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.normal),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 12,
+                            ),
+                            Flexible(
+                              child: SizedBox(
+                                child: GridView.count(
+                                  physics: new NeverScrollableScrollPhysics(),
+                                  crossAxisCount: 5,
+                                  mainAxisSpacing: 4,
+                                  crossAxisSpacing: 3,
+                                  children: avatarList,
                                 ),
                               ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 12,
-                          ),
-                          Flexible(
-                            child: SizedBox(
-                              child: GridView.count(
-                                physics: new NeverScrollableScrollPhysics(),
-                                crossAxisCount: 5,
-                                mainAxisSpacing: 4,
-                                crossAxisSpacing: 3,
-                                children: avatarList,
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                      height: 62 +
-                          (avatarList.length / 5).ceil().toDouble() *
-                              (screenSize.height / factor),
-                      padding: EdgeInsets.all(15),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColorDark,
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(8.0),
-                            bottomLeft: Radius.circular(8.0),
-                            bottomRight: Radius.circular(8.0),
-                            topRight: Radius.circular(8.0)),
-                        boxShadow: <BoxShadow>[
-                          BoxShadow(
-                              color: AppTheme.grey.withOpacity(0.2),
-                              offset: Offset(1.1, 1.1),
-                              blurRadius: 10.0),
-                        ],
+                            )
+                          ],
+                        ),
+                        height: 62 +
+                            (avatarList.length / 5).ceil().toDouble() *
+                                (screenSize.height / factor),
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColorDark,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(8.0),
+                              bottomLeft: Radius.circular(8.0),
+                              bottomRight: Radius.circular(8.0),
+                              topRight: Radius.circular(8.0)),
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                                color: AppTheme.grey.withOpacity(0.2),
+                                offset: Offset(1.1, 1.1),
+                                blurRadius: 10.0),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ),
+                    SizedBox(height: 10,),
+                    Container(
+                    height: 300,
+                    width: screenSize.width,
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                      child: SizedBox(
+                        child: GridView.count(
+                          physics: new NeverScrollableScrollPhysics(),
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 50,
+                          crossAxisSpacing: 20,
+                          childAspectRatio: 1.1,
+                          children: [
+                            MyGroup(
+                              groupName: "摄影组",
+                              groupMember: [
+                                "AH",
+                                "MC",
+                                "KD",
+                                "AL",
+                                "TY",
+                              ],
+                              groupNotes: "无",
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
               ),
             ],
           ),
