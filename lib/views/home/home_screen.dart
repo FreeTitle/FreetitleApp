088ef5562989045fdaf11295885d12b4c655342c
mjiang24 <mjiang24@illinois.edu>
2020-07-12 17:40:20 -0500

    merge post

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'dart:ui';
 import 'package:freetitle/app_theme.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:freetitle/views/post/project_post.dart';
-import 'package:freetitle/views/post/event_post.dart';
-import 'package:freetitle/views/post/blog_post.dart';
-import 'package:freetitle/views/post/multiple_photo_post.dart';
-import 'package:freetitle/views/post/single_photo_post.dart';
+import 'package:freetitle/views/post/post_card.dart';
+import 'package:freetitle/views/post/project_post_card.dart';
+import 'package:freetitle/views/post/event_post_card.dart';
+import 'package:freetitle/views/post/blog/blog_post.dart';
+import 'package:freetitle/views/post/multiple/multiple_photo_post.dart';
+import 'package:freetitle/views/post/single/single_photo.dart';
 
 class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
@@ -63,63 +64,47 @@ class _Home extends State<Home> {
           ),
         ),
       body: SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(top: 20),
-            alignment: Alignment.centerLeft,
-            child: Image.asset(
-                'assets/label_event.png',
-                scale: 2,
-              ),),
-          SingleChildScrollView(
-            child: Row(children: <Widget>[
-              EventPost(),
-              EventPost(),
-              EventPost(),
-              EventPost(),
-              EventPost(),
-            ],),
-            scrollDirection: Axis.horizontal,
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 20),
-            alignment: Alignment.centerLeft,
-            child: Image.asset(
-                'assets/label_project.png',
-                scale: 2,
-              ),),
-          Column(
-            children: <Widget>[
-              ProjectPost(),
-              ProjectPost(),
-              ProjectPost(),
-              ProjectPost(),
-              ProjectPost(),
-              SinglePhotoPost(),
-            ],
-          ),
-        ],
-      ),
-    )
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              alignment: Alignment.centerLeft,
+              child: Image.asset(
+                  'assets/label_event.png',
+                  scale: 2,
+                ),),
+            SingleChildScrollView(
+              child: Row(children: <Widget>[
+                EventPostCard(),
+                EventPostCard(),
+                EventPostCard(),
+                EventPostCard(),
+                EventPostCard(),
+              ],),
+              scrollDirection: Axis.horizontal,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              alignment: Alignment.centerLeft,
+              child: Image.asset(
+                  'assets/label_project.png',
+                  scale: 2,
+                ),),
+            Column(
+              children: <Widget>[
+                ProjectPostCard(),
+                ProjectPostCard(),
+                ProjectPostCard(),
+                ProjectPostCard(),
+                ProjectPostCard(),
+                PostCard(type:'single-photo'),
+                PostCard(type:'blog'),
+                PostCard(type:'multi-photo'),
+              ],
+            ),
+          ],
+        ),
+      )
     );
   }
 }
-
-// body: ListView(
-//           padding: const EdgeInsets.all(8),
-//           children: <Widget>[
-//             Card(
-//                 shape: RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(15.0),
-//                 ),
-//                 elevation: 10,
-//                 child: BlogPost()),
-//             Card(
-//                 shape: RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(15.0),
-//                 ),
-//                 elevation: 10,
-//                 child: MultiplePhotoPost())
-//           ],
-//         ));
---
+++ lib/views/post/blog/blog_post.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:freetitle/app_theme.dart';
+
+class BlogPost extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            borderRadius:
+            BorderRadius.all(Radius.circular(8.0)),
+          ),
+          padding: EdgeInsets.only(top: 10),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.asset(
+              'assets/images/blogpost_.png',
+              fit: BoxFit.fill
+            )
+          )
+        ),
+        Container(
+          padding: EdgeInsets.only(top: 10),
+          child: Text(
+            'Un Chien Andalou has no plot in the conventional',
+            style: Theme.of(context).textTheme.bodyText1
+          )
+        ),
+        Container(
+          padding: EdgeInsets.only(top: 10),
+          child: Text(
+            'Un Chien Andalou has no plot in the conventional sense of the word. The chronology of the film is disjointed, jumping from the initial atThe chronology of the film is disjointed, jumping from the initial at…. Read more ',
+            style: TextStyle(
+              fontSize: 10, color: Colors.grey
+            )
+          )
+        ),
+      ],
+    );
+  }
+}
