d6733d0d2da546d5f689e36222824dc16f484f34
rongni <rni4@illinois.edu>
2020-07-18 23:15:32 -0500

    basic

---
+++ lib/views/post/project_post_detail.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:freetitle/app_theme.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ProjectPostDetail extends StatefulWidget {
+  _ProjectPostDetailState createState() => _ProjectPostDetailState();
+}
+
+class _ProjectPostDetailState extends State<ProjectPostDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('post'));
+  }
+}
