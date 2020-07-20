ae517e8cc4c417c2b7e5025266b137a28fadd974
mjiang24 <mjiang24@illinois.edu>
2020-07-20 12:14:45 -0500

    implement like bloc builder

--- lib/views/post/common/post_bottom.dart
+++ lib/views/post/common/post_bottom.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freetitle/bloc/post/like/bloc.dart';
+import 'package:freetitle/model/post_repository.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:toast/toast.dart';
 
 class PostBottom extends StatefulWidget {
+//  PostBottom({Key key, this.likeBloc}) : super(key : key);
+//  final likeBloc;
   _PostBottomState createState() => _PostBottomState();
 }
 
 class _PostBottomState extends State<PostBottom> {
+
+  LikeBloc _likeBloc;
+
+  @override
+  void dispose() {
+    _likeBloc.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
+    setState(() {
+      //TODO change this
+      _likeBloc = LikeBloc(postRepository: PostRepository(), postID: "WEtwpUukkeSOYECPt8dG");
+    });
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
@@ -57,30 +75,86 @@ class _PostBottomState extends State<PostBottom> {
             ),
           ],
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              child: IconButton(
-                iconSize: 20,
-                icon: Icon(Icons.thumb_up),
-                onPressed: () {},
-              ),
-            ),
-            Text(
-              "44 likes",
-              style: GoogleFonts.galdeano(
-                textStyle: Theme.of(context).textTheme.bodyText1.merge(
-                  TextStyle(
-                    fontSize: 13,
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ),
-            ),
-          ],
+        BlocProvider<LikeBloc>(
+          bloc: _likeBloc,
+          child: LikeButton()
         )
       ],
     );
   }
+}
+
+class LikeButton extends StatefulWidget {
+  _LikeButtonState createState() => _LikeButtonState();
+}
+
+class _LikeButtonState extends State<LikeButton> {
+
+  LikeBloc _likeBloc;
+  bool isLiked = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _likeBloc = BlocProvider.of<LikeBloc>(context);
+    BlocProvider.of<LikeBloc>(context).dispatch(PostLoaded());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener(
+      bloc: _likeBloc,
+      listener: (BuildContext context, LikeState state) {
+        if(state.isLike){
+          isLiked = true;
+          setState(() {
+
+          });
+        }
+        else if(state.isUnlike){
+          isLiked = false;
+          setState(() {
+
+          });
+        }
+        else if(state.failure_LIKE_PRESSED) {
+          Toast.show("Like failed", context, duration: Toast.LENGTH_SHORT, gravity:  Toast.BOTTOM);
+        }
+      },
+      child: BlocBuilder(
+        bloc: _likeBloc,
+        builder: (BuildContext context, LikeState state) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: IconButton(
+                  iconSize: 20,
+                  icon: isLiked ? Icon(Icons.favorite) : Icon(Icons.favorite_border),
+                  onPressed: () {
+                    isLiked ? BlocProvider.of<LikeBloc>(context).dispatch(UnlikePressed(),) : BlocProvider.of<LikeBloc>(context).dispatch(LikePressed(),);
+//                    setState(() {
+//
+//                    });
+                  },
+                ),
+              ),
+              Text(
+                "44 likes",
+                style: GoogleFonts.galdeano(
+                  textStyle: Theme.of(context).textTheme.bodyText1.merge(
+                    TextStyle(
+                      fontSize: 13,
+                      color: Colors.grey[500],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
 }
