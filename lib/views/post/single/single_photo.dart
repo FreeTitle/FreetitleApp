44e93b4e9cd9ada2d550daa0ca11d492329a6c20
mjiang24 <mjiang24@illinois.edu>
2020-07-22 22:06:29 -0500

    show data on card

--- lib/views/post/single/single_photo.dart
+++ lib/views/post/single/single_photo.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:freetitle/app_theme.dart';
+import 'package:freetitle/model/post_repository.dart';
 import 'package:freetitle/views/post/common/post_bottom.dart';
 import 'package:freetitle/views/post/common/post_title.dart';
 import 'package:freetitle/views/post_detail/blog_post_detail.dart';
@@ -10,6 +11,11 @@ import 'package:google_fonts/google_fonts.dart';
 //}
 
 class SinglePhoto extends StatelessWidget {
+
+  SinglePhoto({Key key, this.postData}) : super(key: key);
+
+  final PostModel postData;
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -25,7 +31,7 @@ class SinglePhoto extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Text(
-            "My favorite movie",
+            postData.content,
             style: GoogleFonts.galdeano(
               textStyle: Theme.of(context).textTheme.bodyText1.merge(
                 TextStyle(
@@ -42,7 +48,7 @@ class SinglePhoto extends StatelessWidget {
             width: 400,
             child: ClipRRect(
               borderRadius: const BorderRadius.all(Radius.circular(13.0)),
-              child: Image.asset('assets/placeholders/event.png', fit: BoxFit.cover),
+              child: Image.network(postData.images[0], fit: BoxFit.cover),
             ),
           ),
         ],
